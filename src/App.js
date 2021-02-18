2ec93823b446324e7d6ca63067007c54756723cb
Marlon Strong <tsenane14@gmail.com>
2021-02-19 01:27:20 +0200

    added globastyles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Hero from "./Pages/Hero";
-
+import GlobalStyles from './global.js'
 
 function App() {
   return (
     <>
       <Hero/>
+      <GlobalStyles/>
     </>
   );
 }
--- src/Pages/Hero/index.js
+++ src/Pages/Hero/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Hero() {
     return (
         <div>
-            
+            <h1>Hello World</h1>
         </div>
     )
 }
