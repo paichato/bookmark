785660b1c485057fa79c86cf5bfdcc54ea2ec991
Marlon Strong <tsenane14@gmail.com>
2021-02-19 01:44:05 +0200

    setted globastyles

---
+++ src/global.js
@@ -0,0 +1,40 @@
+
+
+import {createGlobalStyle} from 'styled-components';
+
+
+
+export default createGlobalStyle`
+
+
+*{
+    box-sizing:border-box;
+    padding:0;
+    margin:0;
+    font-family: 'Rubik', sans-serif;
+    border:0;
+    background:white;
+}
+
+body{
+    padding: 5vh 10vw 0px 10vw;
+}
+
+html{
+    max-width:100vw;
+    max-height:100vh;
+}
+
+body{
+    font-size:18px;
+}
+
+#root{
+    --soft-blue:hsl(231, 69%, 60%);
+    --soft-red:hsl(0, 94%, 66%);
+    --grayish-blue:hsl(229, 8%, 60%);
+    -very-dark-blue:hsl(229, 31%, 21%);
+}
+
+
+`
