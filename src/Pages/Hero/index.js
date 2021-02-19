5b135f2d2ff331629323e65e5bde5839073f6102
Marlon Strong <tsenane14@gmail.com>
2021-02-19 02:03:58 +0200

    added material-ui/core -D & mui-treasury -D

--- src/Pages/Hero/index.js
+++ src/Pages/Hero/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import {NavMenu, NavItem} from '@mui-treasury/components/menu/navigation'
+import {usePlain} from '@mui-treasury/components'
 
 function Hero() {
     return (
         <div>
             <h1>Hello World</h1>
+            <NavBar>
+                <NavLogo>
+
+                </NavLogo>
+                <NavLinks>
+                    <NavMenu></NavMenu>
+                </NavLinks>
+            </NavBar>
         </div>
     )
 }
