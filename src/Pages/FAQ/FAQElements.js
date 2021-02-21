17c0712200648646ae8fcd04aa0931912b563517
Marlon Strong <tsenane14@gmail.com>
2021-02-21 02:19:03 +0200

    Added base from accordaion styles

--- src/Pages/FAQ/FAQElements.js
+++ src/Pages/FAQ/FAQElements.js
@@ -10,8 +10,36 @@ justify-content:center;
 align-items:center;
 text-align:center;
 height:900px;
+
+
+.accordion{
+    /* width:40vw; */
+    
+    border-top:1px solid rgba(0,0,0,0.1);
+    border-bottom:1px solid rgba(0,0,0,0.1);
+    box-shadow:none;
+    background:white;
+    color:var(--very-dark-blue);
+    
+}
+.Mui-expanded .exp{
+    fill:#ff4569;
+}
+
+h4{
+    font-weight:400;
+
+    &:hover{
+        color:#ff4569;
+    }
+}
+p{
+    color:var(--grayish-blue);
+}
+
 `
 
 export const FAQContent=styled.div`
-
+width:30vw;
+margin-top:50px;
 `
--- src/Pages/FAQ/index.js
+++ src/Pages/FAQ/index.js
@@ -1,22 +1,28 @@
-import { Accordion, Button } from '@material-ui/core'
+import { Accordion, AccordionDetails, AccordionSummary, Button } from '@material-ui/core'
+import { ExpandMore } from '@material-ui/icons'
 import React from 'react'
 import { FeaturesHeader } from '../Features/FeaturesElements'
-import { FAQSection } from './FAQElements'
+import { FAQContent, FAQSection } from './FAQElements'
 
 function FAQ() {
     return (
         <>
         <FAQSection>
         <FeaturesHeader>
-                <h1>Features</h1>
-                <p>Our aim is to make it 
-                    quick and easy for you to access
-                    your favourite websites. Your bookmarks
-                    sync between your devices so you
-                    can access them on the go.</p>
+                <h1>Frequently Asked QUestions</h1>
+                <p>Here are some of our FAQs.If
+                    you have any other questions you'd like
+                    answered please feel free to email us.
+                </p>
             </FeaturesHeader>
             <FAQContent>
-                <Accordion>
+                <Accordion className="accordion">
+                    <AccordionSummary className="accordion" expandIcon={<ExpandMore className="exp" color="primary"/> }>
+              <h4>What is Bookmark</h4>
+          </AccordionSummary>
+          <AccordionDetails>
+              <p>bla bla bla</p>
+          </AccordionDetails>
 
                 </Accordion>
                 <Button variant="contained" color='primary'>More info</Button>
